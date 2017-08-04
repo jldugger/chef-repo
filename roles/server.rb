457f723e6ddaa9d4130044f200a92f13e97cde5c
Justin Dugger <jldugger@gmail.com>
2017-08-03 18:58:51 -0700

    upgrade irc recipe to shell server

--- roles/server.rb
+++ roles/server.rb
@@ -1,7 +1,7 @@
 name 'server'
 description 'Server role'
 run_list ['recipe[pwnguin::server]',
-          'recipe[pwnguin::irc',
+          'recipe[pwnguin::shell',
           'recipe[pwnguin::database]',
           'recipe[pwnguin::davical]',
           'recipe[pwnguin::photologue]']
