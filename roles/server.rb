b50904f74088d24c0948d61fecf28b4a073a59f2
Justin Dugger <jldugger@gmail.com>
2020-01-06 22:29:28 -0800

    remove shell from webserver role

--- roles/server.rb
+++ roles/server.rb
@@ -1,7 +1,6 @@
 name 'server'
 description 'Server role'
 run_list ['recipe[pwnguin::server]',
-          'recipe[pwnguin::shell]',
           'recipe[pwnguin::database]',
           'recipe[pwnguin::davical]',
           'recipe[pwnguin::photologue]',
