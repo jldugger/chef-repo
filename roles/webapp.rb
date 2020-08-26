2476a228dc8418dd84ead63d635b4e36f4cae2c3
Justin Dugger <jldugger@gmail.com>
2020-08-25 22:28:46 -0700

    seperate webapp role

---
+++ roles/webapp.rb
@@ -0,0 +1,8 @@
+name 'server'
+description 'Server role'
+run_list ['recipe[base-cookbook]',
+          'recipe[pwnguin::server]',
+          'recipe[pwnguin::default]',
+          'recipe[pwnguin::davical]',
+          'recipe[pwnguin::photologue]',
+]
